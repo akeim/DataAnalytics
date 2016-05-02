b93f6b8d60cea5ea688a8c1d1053968e12205ed2
Alyse Keim <akeim@users.noreply.github.com>
2016-05-02 12:19:13 -0400

    Adding In lesson files

---
+++ 2_nycSubwayStats/inLesson/Pandas Introduction.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+'''
+http://www.gregreda.com/2013/10/26/intro-to-pandas-data-structures/
+'''
+
+if False:
+    series = pd.Series(['Dave', 'Cheng-Han', 'Udacity', 42, -1789710578])
+    print series
+
+
+if False:
+    series = pd.Series(['Dave', 'Cheng-Han', 359, 9001],
+                       index=['Instructor', 'Curriculum Manager',
+                              'Course Number', 'Power Level'])
+    print series
+
+
+if False:
+    series = pd.Series(['Dave', 'Cheng-Han', 359, 9001],
+                       index=['Instructor', 'Curriculum Manager',
+                              'Course Number', 'Power Level'])
+    print series['Instructor']
+    print ""
+    print series[['Instructor', 'Curriculum Manager', 'Course Number']]
+
+
+if False:
+    cuteness = pd.Series([1, 2, 3, 4, 5], index=['Cockroach', 'Fish', 'Mini Pig',
+                                                 'Puppy', 'Kitten'])
+    print cuteness > 3
+    print ""
+    print cuteness[cuteness > 3]
+
+
+if False:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print football
+
+
+if False:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print football.dtypes
+    print ""
+    print football.describe()
+    print ""
+    print football.head()
+    print ""
+    print football.tail()
+
+if False:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print football['year']
+    print ''
+    print football.year  # shorthand for football['year']
+    print ''
+    print football[['year', 'wins', 'losses']]
+
+
+if False:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print football.iloc[[0]]
+    print ""
+    print football.loc[[0]]
+    print ""
+    print football[3:5]
+    print ""
+    print football[football.wins > 10]
+    print ""
+    print football[(football.wins > 10) & (football.team == "Packers")]
