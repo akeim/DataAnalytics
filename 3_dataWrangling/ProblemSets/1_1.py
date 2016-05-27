3359a926c3c426fa3af0b3c30553b66982af89d2
Alyse Keim <akeim@users.noreply.github.com>
2016-05-26 20:13:03 -0400

    Adding problemsets

---
+++ 3_dataWrangling/ProblemSets/1_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import csv
+import os
+
+DATADIR = ""
+DATAFILE = "745090.csv"
+
+
+def parse_file(datafile):
+    name = ""
+    data = []
+    with open(datafile,'rb') as f:
+        reader = csv.reader(f, delimiter=',')
+        for line in reader:
+            if reader.line_num == 1:
+               name = line[1]
+            elif reader.line_num == 2:
+               pass
+            else:
+               data.append(line)
+    return (name, data)
+
+
+def test():
+    datafile = os.path.join(DATADIR, DATAFILE)
+    name, data = parse_file(datafile)
+
+    assert name == "MOUNTAIN VIEW MOFFETT FLD NAS"
+    assert data[0][1] == "01:00"
+    assert data[2][0] == "01/01/2005"
+    assert data[2][5] == "2"
+
+
+if __name__ == "__main__":
+    test()
