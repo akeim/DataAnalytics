792bbda7b6c2bc64e04e3c25cc37042d64d9540d
Alyse Keim <akeim@users.noreply.github.com>
2016-05-31 13:19:52 -0400

    Adding problemsets

---
+++ 3_dataWrangling/ProblemSets/2_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_carriers(page):
+    data = []
+
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html)
+        carriers = soup.find(id="CarrierList").find_all('option')
+        for airline in carriers:
+            data.append(airline["value"])
+        for value in data:
+            if "All" in value:
+                data.remove(value)
+
+    return data[1:]
+
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+    airport = data["airport"]
+    carrier = data["carrier"]
+
+    r = requests.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+                    data={'AirportList': airport,
+                          'CarrierList': carrier,
+                          'Submit': 'Submit',
+                          "__EVENTTARGET": "",
+                          "__EVENTARGUMENT": "",
+                          "__EVENTVALIDATION": eventvalidation,
+                          "__VIEWSTATE": viewstate
+                    })
+
+    return r.text
+
+
+def test():
+    data = extract_carriers(html_page)
+    assert len(data) == 16
+    assert "FL" in data
+    assert "NK" in data
+
+test()
